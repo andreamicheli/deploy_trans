1524fd4dfeed2625618888b59eccae28ff7d7f34
andreamicheli <s288281@studenti.polito.it>
2022-06-21 11:26:50 +0200

    fixed core buttons

--- src/Core.js
+++ src/Core.js
@@ -35,11 +35,29 @@ function Core() {
   const [output, setoutput] = useState(0)
   const [next, setNext] = useState(0)
 
+  useEffect(() => {
+    let timeout = 0;
+    if (output===2){
+       timeout = setTimeout(() => {
+        setoutput(3)}, 1000);
+    }
+  
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [output])
+  
+
+
   return (
     <>
-    <div className={output===2 ? 'absolute top-0 left-0 flex justify-center items-center' : 'hidden'}>
+
+    <audio src='Binary.mp3' autoPlay className='hidden'/>
+
+    <div className={output>=2 ? 'absolute z-20 top-0 left-0 flex justify-center items-center w-screen h-screen' : 'hidden'}>
       <img src={'/stopgif.gif'} alt='stop' className="w-full h-full"/>
-      <div className='absolute text-center text-red-500 animate-pulse font-VT323 text-6xl'>FUTURA È STATA DISATTIVATA</div>
+      <div className='absolute z-20 text-center text-red-500 animate-pulse font-VT323 text-6xl'>FUTURA È STATA DISATTIVATA</div>
+      <div className={output===3 ? 'absolute z-30 w-4/5 bg-green-400 h-full' : 'hidden'}>video</div>
     </div>
 
     <div className='bg-black w-screen h-screen flex flex-col text-white'>
@@ -58,8 +76,8 @@ function Core() {
           {next >= 10 ? <><Parola sentence={'|'} setNext={setNext}/><br /></> : <></>}
           {next >= 11 ? <><Parola sentence={'|'} setNext={setNext}/><br /></> : <></>}
           {next >= 11 ? <><Parola sentence={'|'} setNext={setNext}/><br /></> : <></>}
-          {next >= 12 ? <><Parola sentence={'inserire comandi:'} setNext={setNext}/></> : <></>}
-          {next >= 13 ? <><div className='w-1/2 pb-10 flex'>
+          {next >= 12 ? <><Parola sentence={'inserire comandi:'} setNext={setNext}/><br /></> : <></>}
+          {next >= 13 ? <><div className='w-1/2 pb-10 flex z-10'>
             <button className='bg-transparent hover:bg-cyan-300 hover:text-black text-cyan-300 py-2 px-4 mr-4 border-2 border-cyan-300' onClick={()=>{setoutput(1)}}>START</button>
             <button className='bg-transparent hover:bg-red-400 hover:text-black text-cyan-300 py-2 px-4 border-2 border-cyan-300 hover:border-red-400' onClick={()=>{setoutput(2)}}>STOP</button>
             <button></button></div></> : <></>}
@@ -78,15 +96,15 @@ function Core() {
           </Canvas>
         </div>
       </div>
-      <div className='flex flex-row w-full h-1/2'>
-      <div className='w-1/2 flex items-center'>
+      <div className='flex flex-row w-full h-1/2 items-end pb-6 justify-between'>
+      <div className={'w-1/2 flex h-2/3'}>
           {/* <img src={'/audiohack.gif'} alt='hacking1' className="h-2/3 pr-20"/> */}
-          <img src={'/russiahack.gif'} alt='hacking2' className="w-2/3 pl-20"/>
+          <img src={'/russiahack.gif'} alt='hacking2' className="h-full pl-20"/>
         </div>
 
         <div className='w-1/2 flex items-center justify-center'>
-          <img src={'/icegif-174.gif'} alt='hacking3' className="h-2/3 pr-20"/>
-          <img src={'/icegif-174.gif'} alt='hacking4' className="h-1/3 pr-20"/>
+          <img src={'/icegif-174.gif'} alt='hacking3' className="w-1/3 pr-20"/>
+          <img src={'/icegif-174.gif'} alt='hacking4' className="w-1/2 pr-20"/>
         </div>
       </div>
     </div>
